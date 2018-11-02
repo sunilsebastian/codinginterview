cf244c7d1f85decbffcf56de4dd3f78a1cd8ff29
sunil sebastian <sunil.sebastian@gmail.com>
2018-11-01 21:10:17 -0700

    removing unused code

--- ArrayProblems/ArrayProblems/PrintMatrixDiagonally.cs
+++ ArrayProblems/ArrayProblems/PrintMatrixDiagonally.cs
@@ -34,17 +34,7 @@ namespace ArrayProblems
                 }
             }
 
-            for (int j = 1; j < mat.GetLength(1); j++)
-            {
-                int i = mat.GetLength(0) - 1; //row
-                int m = j; //column;
-                while (m < mat.GetLength(1) && i >= 0)
-                {
-                    Console.Write(mat[i, m] + " ");
-                    i--;
-                    m++;
-                }
-            }
+           
         }
 
     }
