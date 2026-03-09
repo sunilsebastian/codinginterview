3f24a95c852bb6b8fa7397ae5c3ff83039b2839f
sunilsebastian <40777304+sunilsebastian@users.noreply.github.com>
2026-03-08 17:10:39 -0700

    Update IntToString.cs

--- StringProblems/IntToString.cs
+++ StringProblems/IntToString.cs
@@ -8,6 +8,30 @@ namespace StringProblems
 {
     public class IntToString
     {
+        public static string IntToString(int num)
+        {
+            if (num == 0) return "0";
+        
+            bool negative = num < 0;
+            if (negative) num = -num;
+        
+            StringBuilder sb = new StringBuilder();
+        
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sb.Append((char)(digit + '0'));
+                num /= 10;
+            }
+        
+            if (negative)
+                sb.Append('-');
+        
+            char[] arr = sb.ToString().ToCharArray();
+            Array.Reverse(arr);
+        
+            return new string(arr);
+        }
         public static string Convert(int n)
         {
            StringBuilder numString = new StringBuilder();
