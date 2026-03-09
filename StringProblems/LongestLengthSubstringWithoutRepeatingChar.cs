3f4cac054093e2b7b85ff8b1622ca985f7239c58
sunilsebastian <40777304+sunilsebastian@users.noreply.github.com>
2026-03-08 17:58:56 -0700

    Update LongestLengthSubstringWithoutRepeatingChar.cs

--- StringProblems/LongestLengthSubstringWithoutRepeatingChar.cs
+++ StringProblems/LongestLengthSubstringWithoutRepeatingChar.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace StringProblems
 {
+    //3
     //logest substring without repeating characters
     //sliding window
     public class LongestLengthSubstringWithoutRepeatingChar
